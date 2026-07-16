20f3f9e28211c449f16fbb72c73e69ed380e477f
Benjamin <kipkoech9275@gmail.com>
2026-07-16 15:18:25 +0300

    fix: remove the duplicate interfaces

--- src/features/movies/types.ts
+++ src/features/movies/types.ts
@@ -1,19 +1,4 @@
-export interface Movie {
-  adult: boolean;
-  backdrop_path: string | null;
-  genre_ids: number[];
-  id: number;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: number;
-  poster_path: string | null;
-  release_date: string;
-  title: string;
-  video: boolean; 
-  vote_average: number;
-  vote_count: number;
-}
+
 
 export interface SearchMoviesResponse {
   page: number;
@@ -46,7 +31,7 @@ export interface SpokenLanguage {
   name: string;
 }
 
-export interface MovieDetailsResponse {
+export interface Movie {
   adult: boolean;
   backdrop_path: string | null; 
   belongs_to_collection: {
@@ -78,13 +63,6 @@ export interface MovieDetailsResponse {
   video: boolean;
   vote_average: number;
   vote_count: number;
-}
-
-export interface SearchMoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
 }
 
 export type MoviesResponse = SearchMoviesResponse;
