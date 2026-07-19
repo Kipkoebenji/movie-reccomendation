fa1ffe432033bdd2ad5b2e84f00d71ae075e49c5
Benjamin <kipkoech9275@gmail.com>
2026-07-19 11:43:11 +0300

    docs: add a comment about \/api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: "/api",  //it's talking to your own Next.js server, not TMDB. src/app/api/movies/route.ts will handle the request and call TMDB API
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
