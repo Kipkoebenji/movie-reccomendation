c6ecf3c7f38f56c70cfa354341522dc79badb4f2
Benjamin <kipkoech9275@gmail.com>
2026-07-19 11:41:31 +0300

    fix: change it to a frontend api client

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,30 +1,11 @@
-import axios, { AxiosError, AxiosInstance } from "axios";
+import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
-const ACCESS_TOKEN = process.env.TMDB_ACCESS_TOKEN;
-
-const api: AxiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+const api = axios.create({
+  baseURL: "/api",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
   },
 });
-
-api.interceptors.request.use(
-  (config) => {
-    if (ACCESS_TOKEN) {
-      config.headers.Authorization = `Bearer ${ACCESS_TOKEN}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
-
-api.interceptors.response.use(
-  (response) => response,
-  (error: AxiosError) => Promise.reject(error),
-);
 
 export default api;
